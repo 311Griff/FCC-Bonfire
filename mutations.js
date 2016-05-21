7b94c3010e318967dd7e4fcb4a643ea9dde18548
Damon Savage <damon.savage@clearlink.com>
2016-05-21 11:11:05 -0600

    FCC js files

---
+++ mutations.js
@@ -0,0 +1,13 @@
+//this verifies if all the letters in the second value of an array are contained in the first value
+function mutation(arr) {
+    var a = arr[1].toLowerCase().split('');
+    var c = true;
+    for(var i= 0, length = a.length; i<length; i++) {
+        if(arr[0].toLowerCase().indexOf(a[i], 0) == -1) {
+            c=false;
+        }
+    }
+    return(c) ? true : false;
+}
+
+mutation(["hello", "hey"]);
