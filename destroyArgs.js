abb512475537ce9bcd5611983d6fb882b6d897eb
Damon Savage <damon.savage@clearlink.com>
2016-05-22 16:36:54 -0600

    FCC js files

--- destroyArgs.js
+++ destroyArgs.js
@@ -1,11 +1,11 @@
 function destroyer(arr) {
-    var a = Array.prototype.slice.call(arguments, 1);
+    var a = Array.prototype.slice.call(arguments, 1); //sets the arguments into their own array
     function destroyMe(val) {
-        return a.indexOf(val) === -1;
+        return a.indexOf(val) === -1; //checks to see if array arguments are included in the separate args or not
 
     }
 
-    return arguments[0].filter(destroyMe);
+    return arguments[0].filter(destroyMe); //filters the array values against the arguments.
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
