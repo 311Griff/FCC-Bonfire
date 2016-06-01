fb59c2f342d3aec168d05126683e8cdc3fa5c51a
Damon Savage <damon.savage@clearlink.com>
2016-06-01 16:27:30 -0600

    sum numbers in a range.

---
+++ sumNumbersInRange.js
@@ -0,0 +1,21 @@
+//this function below will receive two numbers in an array and sum all numbers between them including themselves
+
+function sumAll(arr) {
+
+    var max = Math.max(arr[0],arr[1]);
+    var min = Math.min(arr[0],arr[1]);
+    var newNum = [];
+    for(var i =min+1; i<max; i++) {
+        newNum.push(i);
+
+    }
+    var newArr = arr.concat(newNum);
+    var a = newArr.reduce(function(pv, nv, index, newArr) {
+        return pv + nv;
+    });
+
+
+    return a;
+}
+
+sumAll([1, 4]);
