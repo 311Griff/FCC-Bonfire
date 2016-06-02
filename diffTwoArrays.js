8d23761e1fafa2ff269124c6fa6cc06f9760742c
Damon Savage <damon.savage@clearlink.com>
2016-06-02 11:25:04 -0600

    diff of two arrays function

---
+++ diffTwoArrays.js
@@ -0,0 +1,13 @@
+//this function takes two array arguments and returns the values that are unique in each array.
+function diffArray(arr1, arr2) {
+
+    var both = arr1.concat(arr2);
+    return both.filter(function (val) {
+        if (arr2.indexOf(val) === -1 || arr1.indexOf(val) === -1) {
+            return val;
+        }
+    });
+}
+
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
