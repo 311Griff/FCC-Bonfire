f876ad489c6588355dc5c172076b79d7fea24c3a
Damon Savage <damon.savage@clearlink.com>
2016-06-09 23:22:03 -0600

    sorted Unions - find the unique values in an array and make an array of them

---
+++ sortedUnion.js
@@ -0,0 +1,22 @@
+//this function will take an array of array, etc, and find the unique characters and combine them into a new array
+function uniteUnique(arr) {
+    var args = Array.prototype.slice.call(arguments);
+    var first = args[0];
+    var arrays = [];
+    var bigArray;
+
+    args.reduce(function(pv, cv) {
+        return cv.map(function(val){
+
+            if(first.indexOf(val) === -1) {
+                arrays.push(val);
+            }
+        });
+    });
+
+    bigArray = first.concat(arrays);
+
+    return bigArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
