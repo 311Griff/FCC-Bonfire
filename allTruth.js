fe88100342f1c6348ad67d87016e447453def5a6
Damon Savage <damon.savage@clearlink.com>
2016-06-21 10:15:28 -0600

    allTruth.js - removed console.log

--- allTruth.js
+++ allTruth.js
@@ -25,3 +25,21 @@ function truthCheck(collection, pre) {
     return overallTruth;
 }
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+//alternate: this is a cleaner alternative to the above code
+/*
+function truthCheck(collection, pre) {
+    var passed =true;
+
+    Object.keys(collection).forEach(function(index){
+       if(collection[index].hasOwnProperty(pre) && collection[index][pre]) {
+
+       } else {
+           passed=false;
+       }
+    });
+
+
+    return passed;
+}*/
