facb8a9ab77bd9a74a28bb90facfed1044cdd5f4
Damon Savage <damon.savage@clearlink.com>
2016-06-26 21:59:14 -0600

    list to array - takes a linked list of objects and pushes the values to an array.

---
+++ listToArray.js
@@ -0,0 +1,20 @@
+//fundamental object linked-list to array in js
+
+function listToArray(list) {
+    'use strict';
+    var arr = [];
+    var x;
+
+    while(list !== null) {
+        x=list.value;
+        arr.push(x);
+        list = list.next;
+    }
+
+}
+
+var list1 = {value: 1, next: {value: 2, next: {value: 3, next: null}}};
+var list2 = {value: "foo", next: {value: "bar", next: null}};
+
+listToArray(list1);
+listToArray(list2);
