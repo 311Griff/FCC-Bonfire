f92650842b775f1a856563b0769c0a5e504c8be7
Damon Savage <damon.savage@clearlink.com>
2016-07-20 14:43:09 -0600

    tic-tac-toe game js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -6,87 +6,91 @@ var usrMarker;
 var cmpMarker;
 //set the board boundaries (array?, objects?)
 var board = document.querySelectorAll('.box');
-var usrArr = [], cmpArr = [];
+var usrArr = [],
+    cmpArr = [];
 
 marker.addEventListener('change', function(e) {
     e.stopPropagation();
-    if(marker.value === 'no') {
-       this.innerHTML = "Please Select One!"
-   }
     usrMarker = marker.value;
-
-    if(usrMarker == 'x') {
-        cmpMarker = 'o';
+    if (usrMarker == 'X') {
+        cmpMarker = 'O';
+        marker.parentElement.setAttribute('class', 'close');
     } else {
-        cmpMarker = 'x';
+        cmpMarker = 'X';
+        marker.parentElement.setAttribute('class', 'close');
     }
 });
 
 var checkWinner = function checkWinner(arr) {
-    var win = this.innerHTML = "win!";
-    switch (arr) {
-        case [0, 1, 2]:
-            return win;
-            break;
-        case [3, 4, 5]:
-            return win;
-            break;
-        case [6, 7, 8]:
-            return win;
-            break;
-        case [2, 4, 6]:
-            return win;
-            break;
-        case [0, 4, 8]:
-            return win;
-            break;
-        default:
-            return false;
+    switch (true) {
+     case arr.indexOf('0')> -1 && arr.indexOf('1') > -1 && arr.indexOf('2') >-1:
+     return 'top row';
+     break;
+     case arr.indexOf('3')> -1 && arr.indexOf('4') > -1 && arr.indexOf('5') >-1:
+     return 'middle row';
+     break;
+     case arr.indexOf('6')> -1 && arr.indexOf('7') > -1 && arr.indexOf('8') >-1:
+     return 'bottom row';
+     break;
+     case arr.indexOf('2')> -1 && arr.indexOf('4') > -1 && arr.indexOf('6') >-1:
+     return 'diagonal top right to bottom left';
+     break;
+     case arr.indexOf('0')> -1 && arr.indexOf('4') > -1 && arr.indexOf('8') >-1:
+     return 'diagonal top left to bottom right';
+     break;
+     case arr.indexOf('2')> -1 && arr.indexOf('5') > -1 && arr.indexOf('8') >-1:
+     return 'right column';
+     break;
+     case arr.indexOf('1')> -1 && arr.indexOf('4') > -1 && arr.indexOf('7') >-1:
+     return 'middle column';
+     break;
+     case arr.indexOf('0')> -1 && arr.indexOf('3') > -1 && arr.indexOf('6') >-1:
+     return 'left column';
+     break;
+     default:
+     return;
 
-    }
+     }
+
 };
 
-//user goes...
+//user's play...
 board.forEach(function(aBox) {
-    var win = checkWinner(usrArr);
-    if(!win) {
-       aBox.addEventListener('click', function (e) {
-           e.stopPropagation();
-           if (usrMarker === 'no') {
-               this.innerHTML = "Please select a marker";
-           } else {
-               this.innerHTML = usrMarker;
-           }
-           usrArr.push(this.getAttribute('data-value'));
-           this.setAttribute('name', 'used');
-           computerMove(usrArr);
-       });
-   } else {
-
-        //done!
-    }
+    aBox.addEventListener('click', function(e) {
+        e.stopPropagation();
+        if (this.getAttribute('name')!== 'used'){
+            this.innerHTML = usrMarker;
+            usrArr.push(this.getAttribute('data-value'));
+            usrArr.sort();
+            this.setAttribute('name', 'used');
+        } else {
+            //do nothing
+        }
+        //check for a winner
+        var win = checkWinner(usrArr);
+        if (win !== undefined) {
+            alert("winner! " + usrMarker +" "+  win);
+            location.reload();
+        } else {
+            computerMove();
+        }
+    });
 
 });
 
-
 //computer Go...
-function computerMove(arr) {
-    console.log(arr);
+function computerMove() {
+    var canUse = document.getElementsByName('notused');
+    console.log(canUse);
+    var random = Math.floor(Math.random() * (canUse.length - 0) + 0);
+    var play = canUse[random];
+    play.innerHTML = cmpMarker;
+    play.setAttribute('name', 'used');
+    cmpArr.push(play.getAttribute('data-value'));
+    //check for a winner
+    var win = checkWinner(cmpArr);
+    if (win !== undefined) {
+        alert("winner! " + cmpMarker +" "+  win);
+    }
+
 };
-
-
-/*Tl, Tm, Tr
- Ml, Mm, Mr
- Bl, Bm, Br*/
-
-//user goes first, then computer, then user....
-
-
-
-//selection made - user
-
-//computer selection open
-
-//computer makes selection
-
-//user selection open
